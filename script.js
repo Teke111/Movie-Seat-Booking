d109a8b1f0e6c1861324f4d53cc8eba9983809f0
Kolawole Emmanuel Teke <kolawoleteke96@gmail.com>
2023-11-05 19:45:17 +0100

    I made new changes

--- script.js
+++ script.js
@@ -1,38 +1,82 @@
-// FINDING THE WEBSITE URL
-let url = window.location.href;
-// console.log(url);
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const price = document.getElementById('price');
 
-// 
-let url_segment = url.split('?');
-// console.log(url_segment[1]); 
+const movieSelect = document.getElementById('movie');
+let ticketPrice = +movieSelect.value;
 
+const populateUI = () => {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
 
-
-let play_btn = document.getElementById('play');
-let video = document.getElementById('video');
-
-
-play_btn.addEventListener('click', () => {
-    if (video.paused) {
-        video.play();
-        video.style.display = 'unset';
-        play_btn.classList.remove('bi-play-fill');
-        play_btn.classList.add('bi-pause');
-    } else {
-        video.pause();
-        video.style.display = 'none';
-        play_btn.classList.add('bi-play-fill');
-        play_btn.classList.remove('bi-pause');
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
     }
-})
 
-video.addEventListener('ended', () => {
-    video.play();
-})
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    const selectedMoviePrice = localStorage.getItem('selectedMoviePrice');
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+
+    if (selectedMoviePrice !== null) {
+        count.innerText = selectedSeats.length;
+        price.innerText = selectedSeats.length * +selectedMoviePrice;
+    }
+};
+
+populateUI();
+
+selectedMovie = (movieIndex, moviePrice) => {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+};
+
+const updateSelectedSeatsCount = () => {
+    const selectedSeats = document.querySelectorAll('.row .selected');
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    price.innerText = selectedSeatsCount * ticketPrice;
+};
+
+// Seat select event
+container.addEventListener('click', e => {
+    if (
+        e.target.classList.contains('seat') &&
+        !e.target.classList.contains('occupied')
+
+    ) {
+        e.target.classList.toggle('selected');
+
+        updateSelectedSeatsCount();
+    }
+});
+
+// Movie select event
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    console.log(ticketPrice)
+    selectedMovie(e.target.selectedIndex, e.target.value);
+
+    updateSelectedSeatsCount();
+});
 
 
 
-//TO GET SURRENT DATE 
+// DATE
+
+
 let date = new Date();
 let main_date = date.getDate();
 // console.log((main_date))
@@ -43,56 +87,30 @@ Array.from(document.getElementsByClassName('date_point')).forEach((el) => {
     }
 })
 
-let pvr = [
-    {
-        pvr: 'PVR Vegus',
-        movie: 'Jawan',
-        loc: 'Dwarka Sector 14 , New Delhi',
-        audi: 1,
-        type: '4DX',
-        series: ['J', 'H', 'F', 'E', 'D', 'C', 'B', 'A'],
-        row_section: 3,
-        seat: 24,
-        j: [2, 6, 24, 23, 7, 16, 17, 18, 19, 13, 12],
-        h: [1, 2, 78, 20, 23, 8, 11, 18, 19, 13, 12],
-        f: [5, 6, 15, 17, 18],
-        e: [2, 7, 8, 17, 18],
-        d: [5, 16, 15, 23, 22],
-        c: [1, 2, 11, 12, 19],
-        b: [8, 5],
-        a: [],
-        price: [800, 800, 560, 560, 560, 560, 430, 430],
-        date: 24,
-        img: 'img/jawan.jpg',
-        video: 'video/Jawan Official Trailer-(HDvideo9).mp4',
-        background: 'img/bg.png',
-    },
-    {
-        pvr: 'PVR Vegus',
-        movie: 'Gadar2',
-        loc: 'Dwarka Sector 14 , New Delhi',
-        audi: 2,
-        type: '4DX',
-        series: ['J', 'H', 'F', 'E', 'D', 'C', 'B', 'A'],
-        row_section: 3,
-        seat: 24,
-        j: [3, 8, 21, 22, 9, 13, 16, 18, 20, 12, 15],
-        h: [5, 6, 8, 19, 24, 8, 13, 19, 21, 12, 11],
-        f: [1, 3, 13, 15, 22],
-        e: [3, 9, 10, 20, 24],
-        d: [7, 12, 9, 21, 20],
-        c: [6, 8, 11, 12, 19],
-        b: [8, 5, 12, 13, 14],
-        a: [],
-        price: [800, 800, 560, 560, 560, 560, 430, 430],
-        date: 24,
-        img: 'img/Gadar2.jpg',
-        video: 'video/Gadar2 Official Trailer - 11th August - Sunny Deol - Ameesha Patel - Anil Sharma - Zee Studios.mp4',
-        background: 'img/gadar_bg.jpg',
-    },
-]
+
+let offDate = () => {
+    Array.from(document.getElementsByClassName('date_point')).forEach(el => {
+        el.classList.remove('h6_active');
+    })
+}
+
+Array.from(document.getElementsByClassName('date_point')).forEach(el => {
+    el.addEventListener('click', () => {
+        if (el.innerText > date.getDate() - 1) {
+            offDate();
+            el.classList.add('h6_active');
+            // main_date = +el.innerText;
+            // document.getElementById('chair').innerHTML = '';
+            // let data = pvr.filter(obj => obj.date == main_date && obj.movie == url_segment[1]);
+            // console.log(data);
+            // addSeats(data)
+        }
+    })
+})
 
 
-// let data = pvr.filter(obj => obj.date == main_date && obj.movie == url_segment[1]);
-let data = pvr.filter(obj => obj.date == main_date && obj.movie == url_segment[1]);
-// console.log(data);
+const first = document.getElementById('first').textContent;
+
+document.getElementById("book_ticket_right").addEventListener("click", () => {
+    console.log(first)
+})
